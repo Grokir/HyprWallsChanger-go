10685f59a7f1bf070124a45b22c640a6ee90bcad
Grokir <grokir13@yandex.ru>
2026-02-10 01:04:36 +0300

    ADD: project files

---
+++ image_handler/image_handler.go
@@ -0,0 +1,27 @@
+package imagehandle
+
+import (
+	"image"
+
+	"github.com/anthonynsimon/bild/blur"
+	"github.com/anthonynsimon/bild/imgio"
+)
+
+
+func save_png(filename string, blurred *image.RGBA) bool {
+	if err := imgio.Save(filename, blurred, imgio.PNGEncoder()); err != nil {
+		return false
+	}
+	return true
+}
+
+func save_jpeg(filename string, blurred *image.RGBA) bool {
+	if err := imgio.Save(filename, blurred, imgio.JPEGEncoder(100)); err != nil {
+		return false
+	}
+	return true
+}
+
+func gaussian_blur(image *image.RGBA, blur_radius float64) *image.RGBA {
+	return blur.Gaussian(image, blur_radius)
+}
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"slices"
+
+	"github.com/anthonynsimon/bild/imgio"
+	"github.com/Grokir/HyprWallsChanger-go/image_handler"
+)
+
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("[!] Invalid argument. Run program like: hypr-walls-blur <image>")
+	}
+
+	var filename string = os.Args[1]
+	var format string = filename[len(filename)-3:]
+	var blur_radius float64 = 400.0
+	var save_flag bool = false
+
+	img, err := imgio.Open(filename)
+	if err != nil {
+		log.Fatalf("[!] failed to load image: %v", err)
+	}
+
+	fmt.Println("[*] Start blurring...")
+	blurred_img := image_handler.gaussian_blur(img, blur_radius)
+
+	fmt.Println("[*] Start save image...")
+	if slices.Contains([]string{"jpg", "jpeg"}, format) {
+		save_flag = image_handler.save_jpeg(filename, blurred_img)
+	} else if format == "png" {
+		save_flag = image_handler.save_png(filename, blurred_img)
+	} else {
+		log.Fatalf("[!] Unsupported format \"%s\"", format)
+	}
+	if !save_flag {
+		log.Fatalf("[!] The image is not saved")
+	}
+
+	fmt.Println("[+] Image blurred and saved as blur.png")
+}
