b91440696441f2175caddbe881435a3d46c5abca
Grokir <grokir13@yandex.ru>
2026-02-10 17:05:33 +0300

    ADD: method for formatted path to file

--- config/config.go
+++ config/config.go
@@ -1,1 +1,18 @@
 package config
+
+import (
+	"log"
+	"os"
+)
+
+func FormatPath(path_to_file string) string {
+	var temp_path string = path_to_file
+	if temp_path[0] == '~' {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatalf("failed to get home directory: %w", err)
+		}
+		temp_path = homeDir + temp_path[1:]
+	}
+	return temp_path
+}
