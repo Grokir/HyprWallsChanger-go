d913ab6c9b5fe62d63ac95319f5051f50ad66563
Grokir <grokir13@yandex.ru>
2026-02-12 19:31:19 +0300

    UPD main for new methods

--- main.go
+++ main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"HyprWallsChanger-go/config"
+	"HyprWallsChanger-go/image_handler"
 	"fmt"
 	"log"
 	"os"
 	"slices"
-	"HyprWallsChanger-go/image_handler"
+
+	// "encoding/json"
+
 	"github.com/anthonynsimon/bild/imgio"
 )
+
+/*
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+func main() {
+	jsonData := []byte(`{"item": "laptop", "price": 999.99}`)
+
+	var data map[string]interface{}
+
+	err := json.Unmarshal(jsonData, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Item: %v, Price: %v\n", data["item"], data["price"])
+	// Output: Item: laptop, Price: 999.99
+}
+
+
+*/
 
 func main() {
 	if len(os.Args) != 2 {
@@ -18,8 +49,17 @@ func main() {
 	var format string = filename[len(filename)-3:]
 	var blur_radius float64 = 100.0
 	var save_flag bool = false
+	var paths_to_img map[string]string = config.LoadImagePath("")
 
-	img, err := imgio.Open(filename)
+	paths_to_img["wall_dir"] = config.FormatPath(paths_to_img["wall_dir"])
+	paths_to_img["lock_dir"] = config.FormatPath(paths_to_img["lock_dir"])
+
+	if image_handler.ImageIsExists(paths_to_img["lock_dir"] + "/" + filename){
+		fmt.Println("[*] The image already exists")
+		return
+	}
+
+	img, err := imgio.Open( paths_to_img["wall_dir"] + "/" + filename )
 	if err != nil {
 		log.Fatalf("[!] failed to load image: %v", err)
 	}
@@ -29,9 +69,9 @@ func main() {
 
 	fmt.Println("[*] Start save image...")
 	if slices.Contains([]string{"jpg", "jpeg"}, format) {
-		save_flag = image_handler.SaveJPEG(filename, blurred_img)
+		save_flag = image_handler.SaveJPEG(paths_to_img["lock_dir"] + "/" + filename, blurred_img)
 	} else if format == "png" {
-		save_flag = image_handler.SavePNG(filename, blurred_img)
+		save_flag = image_handler.SavePNG(paths_to_img["lock_dir"] + "/" + filename, blurred_img)
 	} else {
 		log.Fatalf("[!] Unsupported format \"%s\"", format)
 	}
